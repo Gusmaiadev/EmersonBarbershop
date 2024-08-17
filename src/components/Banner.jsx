b3a97c1ce0f7c8cdead8695a25e291ddb7893040
Gustavo Maia <gustadev@yahoo.com>
2024-08-17 17:32:30 -0300

    Ataualização card info

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,8 +1,12 @@
 
 import styles from '../styles/Banner.module.css';
+import IconLocal from '../assets/icone_endereco.png';
+import IconTel  from '../assets/icone_telefone.png' 
+import IconRelo  from '../assets/icone_relogio.png' 
 
 
-function Banner() {
+function Banner(){
+
     return (
         <>
             <div className={styles.container_banner}>
@@ -22,7 +26,31 @@ function Banner() {
                       </div>
                 </div>
                 <div className={styles.container_info}>
-                        ...
+                    <div className={styles.box_info}>
+                        <div className={styles.card_info}>
+                                <img src={IconLocal} alt="Local"/>
+                                <h2>ENDEREÇO</h2>
+                        </div>
+                        <div className={styles.card_info}>
+                                <img src={IconTel} alt="Telefone"/>
+                                <h2>TELEFONE</h2>
+                        </div>
+                        <div className={styles.card_info}>
+                                <img src={IconRelo} alt="Relogio"/>
+                                <h2>HORÁRIO</h2>
+                        </div>
+                    </div>
+                    <div className={styles.box_descr}>
+                        <div className={styles.card_data}>
+                                <p>R. AMAZONAS, 06 - CIDADE SÃO JORGE, SANTO ANDRÉ</p>
+                            </div>
+                            <div className={styles.card_data}>
+                                <p>(11)94246-3329</p>
+                            </div>
+                            <div className={styles.card_data}>
+                                <p>DE TERÇA-FEIRA Á SÁBADO - DAS 9H ÁS 20H</p>
+                            </div> 
+                    </div>
                 </div>
             </div>
         </>
