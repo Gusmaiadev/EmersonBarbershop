f88dc3469ae5b8d457320ee0e79287d8f196adce
Gustavo Maia <gustadev@yahoo.com>
2024-08-23 17:57:49 -0300

    Craiação componente DataBlockThree

---
+++ src/components/DataBlockThree.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from '../styles/DataBlockThree';
+
+
+
+function DataBlockThree(){
+  return (
+    <section className={styles.container_datablockthree}>
+        <div className={styles.box_datablockthree}>
+
+        </div>
+    </section>
+  );
+};
+
+export default DataBlockThree;
--- src/pages/Page.jsx
+++ src/pages/Page.jsx
@@ -2,6 +2,7 @@
 import Banner from "../components/Banner";
 import CaroselTeam from "../components/CaroselTeam";
 import DataBlockOne from "../components/DataBlockOne";
+import DataBlockThree from "../components/DataBlockThree";
 import DataBlockTwo from "../components/DataBlockTwo";
 import Header from "../components/Header";
 import InfoCard from "../components/InfoCard";
@@ -16,6 +17,7 @@ function Page(){
             <InfoCard/>
             <DataBlockTwo/>
             <CaroselTeam/>
+            <DataBlockThree/>
             
         </>
     )
