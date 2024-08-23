c1a828587f5a49eefdf2c99fe446a467e38ca767
Gustavo <gustadev@yahoo.com>
2024-08-23 20:24:04 -0300

    Responsividade DataBlockThree

--- src/components/DataBlockThree.jsx
+++ src/components/DataBlockThree.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from '../styles/DataBlockThree';
+import styles from '../styles/DataBlockThree.module.css';
 
 
 
@@ -7,7 +7,19 @@ function DataBlockThree(){
   return (
     <section className={styles.container_datablockthree}>
         <div className={styles.box_datablockthree}>
-
+          <div className={styles.block_text}>
+            <h2>CORTES DE CABELO PARA TODOS OS ESTILOS</h2>
+            <p>Na Emerson BarberShop, oferecemos uma variedade 
+              de cortes de cabelo, desde o clássico social até o 
+              moderno degradê e low fade. Nossa equipe de barbeiros 
+              especializados está preparada para realizar o corte 
+              perfeito, garantindo qualidade e estilo para todos 
+              os tipos de cabelo. Venha descobrir a excelência 
+              dos nossos serviços de corte de cabelo.</p>
+            <a href="#"><button className={styles.button}>AGENDAR HORÁRIO</button></a>
+          </div>
+          <div className={styles.block_image}>
+        </div>
         </div>
     </section>
   );
